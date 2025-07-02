1fa579ccc13931d55a4268ed5133da8abd864c9b
Fabley <SimpsonGamer@gmail.com>
2025-07-02 17:35:33 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import React from 'react';
+import PokemonFetcher from './PokemonFetcher';
+
+function App() {
+  return (
+    <>
+      <h1>¡Conoce a tus Pokemón!</h1>
+      <PokemonFetcher/>
+    </>
+  )
+}
+
+export default App
